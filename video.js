ea41f0c52d2ecd89460d5f33b4aab71b8fd82a20
Maksen14 <129395768+Maksen14@users.noreply.github.com>
2024-07-17 19:11:01 +0200

    Add files via upload

---
+++ video.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let videoStarted = false;
+
+    document.addEventListener('click', function () {
+        if (videoStarted) return;
+
+        const videoContainer = document.getElementById('videoContainer');
+        const introVideo = document.getElementById('introVideo');
+        const mainContent = document.getElementById('mainContent');
+        const enterText = document.getElementById('enterText');
+
+        // Hide the ENTER text and play the video
+        enterText.style.display = 'none';
+        introVideo.play();
+        videoStarted = true;
+
+        introVideo.onended = function () {
+            videoContainer.style.display = 'none';
+            mainContent.style.display = 'block';
+            document.body.style.overflow = 'auto';  // Make sure this correctly resets the overflow
+        
+            
+        };
+    });
+});
